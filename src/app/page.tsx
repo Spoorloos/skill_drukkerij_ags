2a75c7f2bdeeb5bc1420839be027932ae3202bd5
pythonlearner12 <fhh1914@gmail.com>
2024-11-19 10:35:34 +0100

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
 export default function Home() {
     return (
         <main>
-
+            <table className="w-1/2 border-collapse border border-black table-fixed">
+                <tr className="border border-black">
+                    <th className="border border-black">Monday</th>
+                    <th className="border border-black">Tuesday</th>
+                    <th className="border border-black">Wednesday</th>
+                    <th className="border border-black">Thursday</th>
+                    <th className="border border-black">Friday</th>
+                </tr>
+                <tr className="border border-black">
+                    <td className="border border-black">Alfreds Futterkiste</td>
+                    <td className="border border-black">Maria Anders</td>
+                    <td className="border border-black">Germany</td>
+                </tr>
+                <tr className="border border-black">
+                    <td className="border border-black">Centro comercial Moctezuma</td>
+                    <td className="border border-black">Francisco Chang</td>
+                    <td className="border border-black">Mexico</td>
+                </tr>
+            </table>
         </main>
     );
 }
