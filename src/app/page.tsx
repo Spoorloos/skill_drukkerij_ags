176bfa2b028f0078ca15e448f4242f576e9d8284
Mick Negenman <36791@ma-web.nl>
2024-11-19 10:34:27 +0100

    Added database stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
-export default function Home() {
+"use server";
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!);
+
+export default async function Home() {
+    const { data, error } = await supabase
+        .from("test")
+        .select();
+
     return (
         <main>
-
+            {error ? (
+                <strong className="text-red-600">Failed to get data from database</strong>
+            ) : data.map(row => {
+                return <p>{row.title!}</p>
+            })}
         </main>
     );
 }
