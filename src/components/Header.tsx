aef356724163a4a61bd87c1d0774032fa1d98fbe
Mick Negenman <36791@ma-web.nl>
2024-11-19 09:28:11 +0100

    Initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Header() {
+    return (
+        <header className="p-3 flex justify-between">
+            <h1 className="font-bold">Drukkerij AGS</h1>
+            <nav className="space-x-3">
+                <Link className="nav-item" href="#">Test</Link>
+                <Link className="nav-item" href="#">Test 1</Link>
+                <Link className="nav-item" href="#">Test 2</Link>
+                <Link className="nav-item" href="#">Test 3</Link>
+            </nav>
+        </header>
+    );
+}
