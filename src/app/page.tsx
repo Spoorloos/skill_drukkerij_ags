2b6c8ee01fc3cacc62caede41ae614e0ca7e05fe
pythonlearner12 <fhh1914@gmail.com>
2024-11-28 10:02:35 +0100

    small changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import Calendar from '@/components/Calendar';
+import Email from '@/components/email_test';
 
 export default function Schedule() {
     return (
         <main className="p-8 pb-0 space-y-8">
             <section>
                 <h2 className="pb-4 text-xl font-bold">Schedule</h2>
-                <Calendar/>
+                <Calendar />
+            </section>
+
+            <section>
+                <h2>email test</h2>
+                <Email />
             </section>
         </main>
     );
